3bbb316375f2379dc36ed9d255f09284bf0adb67
Jean Silva <jeanslucasilva@gmail.com>
2021-01-01 10:03:09 -0300

    Finalizando desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const totalIncome = this.transactions.reduce((total, item) => {
+      if (item.type === 'income') {
+        return total + item.value;
+      }
+      return total;
+    }, 0);
+
+    const totalOutcome = this.transactions.reduce((total, item) => {
+      if (item.type === 'outcome') {
+        return total + item.value;
+      }
+      return total;
+    }, 0);
+
+    const total = totalIncome - totalOutcome;
+
+    const balance: Balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
